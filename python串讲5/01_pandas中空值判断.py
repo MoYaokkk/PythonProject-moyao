a39035191c64a31f798c01af624ae12cfd625006
MoYaokkk <3329593940@qq.com>
2026-09-17 23:56:02 +0800

    day4

--- python串讲5/01_pandas中空值判断.py
+++ python串讲5/01_pandas中空值判断.py
@@ -9,14 +9,14 @@ print(pd.NA, type(pd.NA))
 
 # 3.空值判断操作
 # 注意: 一般pandas中使用isna(),notna()判断,而不使用==
-# isna() 判断是否是空值 (推荐)
+# isna()和isnull() 判断是否是空值 (推荐)
 print(pd.isna('null'))  # False
 print(pd.isna(''))  # False
 print(pd.isna(None))    # True
 print(pd.isna(np.nan))    # True
 print(pd.isna(pd.NA))    # True
 print('-------------------------------')
-# notna() 判断是否不是空值
+# notna()和notnull() 判断是否不是空值
 print(pd.notna('null'))  # True
 print(pd.notna(''))  # True
 print(pd.notna(None))    # False
